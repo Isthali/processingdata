342d55e0f5571c85e575618c0afa9d684b2f5bff
joecga4 <joel.arzapalo@pucp.edu.pe>
2025-01-23 16:41:24 -0500

    modified:   test_data.py modified:   test_ledi.py

--- test_data.py
+++ test_data.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import openpyxl as xl
 import chardet
 
 def detect_encoding(file_path):
     with open(file_path, 'rb') as f:
         result = chardet.detect(f.read(10000))  # Analiza las primeras 10,000 bytes
-        return result['encoding']
+        
+    return result['encoding']
     
 # Función para importar datos de un archivo de texto
 def import_test_data_text(file_path, delimiter, variable_names):
@@ -14,6 +16,7 @@ def import_test_data_text(file_path, delimiter, variable_names):
     # Lectura de datos con pandas
     df = pd.read_csv(filepath_or_buffer=file_path, sep=delimiter, names=variable_names, encoding=encoding)
     df = df.dropna()  # Omitir filas con errores de importación o vacías
+    
     return df
 
 # Función para importar datos de un archivo de Excel
@@ -24,4 +27,40 @@ def import_test_data_excel(file_path, sheet_idx, variable_names):
     # Lectura de datos con pandas
     df = pd.read_excel(io=file_path, sheet_name=sheet_idx, names=variable_names, encoding=encoding)
     df = df.dropna()  # Omitir filas con errores de importación o vacías
+    
     return df
+
+# Función para obtener valores para procesamiento
+def get_report_data(file_path, sheet_idx=None, position=(1, 1)):
+    # Abrir el archivo Excel
+    with xl.load_workbook(filename=file_path, keep_vba=True) as wb:
+        # Seleccionar la hoja
+        if sheet_idx:
+            sheet = wb[sheet_idx]
+        else:
+            # Si no se proporciona nombre, usar la primera hoja
+            sheet = wb.worksheets[0]
+        
+        # Obtener el valor de la celda especificada
+        row, column = position
+        val = sheet.cell(row, column).value
+
+    return val
+
+# Función para guardar valor en un archivo Excel
+def write_report_data(file_path, sheet_idx, position, val):
+    # Abrir el archivo Excel
+    with xl.load_workbook(filename=file_path, keep_vba=True) as wb:
+        # Seleccionar la hoja
+        if sheet_idx in wb.sheetnames:
+            sheet = wb[sheet_idx]
+        else:
+            # Crear una nueva hoja
+            sheet = wb.create_sheet(title=sheet_idx)
+        
+        # Guardar el valor en la celda especificada
+        row, column = position
+        sheet.cell(row, column, value=val)
+        
+        # Guardar los cambios
+        wb.save(file_path)
--- test_ledi.py
+++ test_ledi.py
@@ -1,16 +1,16 @@
 import numpy as np
-import openpyxl as xl
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
-from test_data import import_test_data_text, import_test_data_excel
+from test_data import import_test_data_text, import_test_data_excel, get_report_data, write_report_data
 
 class Mechanical_test:
     def __init__(self):
-        self.infle = 'inf-le'
+        self.sample_id = 0
         self.data = pd.DataFrame()
         self.data_process = pd.DataFrame()
         self.data_file = 'data_file'
+        self.report_file = 'report_file'
     
     def get_data(self, data_file, data_source, variable_names):
         self.data_file = data_file
@@ -26,24 +26,29 @@ class Mechanical_test:
             
     def preprocess_data(self):
         return self.data
+    
+    def plot_data(self, x, y, title, xlabel, ylabel, legend, test_name, num_pag, final_pag=False):
+        fig, ax = plt.subplots(figsize=(11.7, 8.3))
+        ax.plot(self.data[x], self.data[y], 'b-', linewidth=2)
+        ax.set_title(title, fontsize=10)
+        ax.set_xlabel(xlabel, fontsize=9)
+        ax.set_ylabel(ylabel, fontsize=9)
+        ax.legend(legend, fontsize=9)
+        ax.grid(visible=True, which='both', linestyle='--')
+        ax.minorticks_on()
+        ax.set_position([0.1, 0.15, 0.7, 0.75])
+        fig.text(0.05, 0.05, f'INF-LE {self.infle}', fontsize=8, horizontalalignment='left')
+        fig.text(0.5, 0.05, f'LEDI-{test_name}', fontsize=8, horizontalalignment='center')
+        fig.text(0.85, 0.05, f'Pág. {num_pag}', fontsize=8, horizontalalignment='right')
 
-    def plot_data(self, x, y, title, xlabel, ylabel, legend, savefig=False, figname='figname'):
-        plt.figure(figsize=(10, 8))
-        plt.plot(self.data[x], self.data[y], 'b-', linewidth=2)
-        plt.title(title, fontsize=10)
-        plt.xlabel(xlabel, fontsize=9)
-        plt.ylabel(ylabel, fontsize=9)
-        plt.legend(legend, fontsize=9)
+        if final_pag:
+            fig.text(0.85, 0.01, 'Fin del informe', fontsize=8, horizontalalignment='right')
         
-        if savefig:
-            plt.savefig(figname + '.png')
-        
-        return plt
+        return fig, ax
 
-class Resistant_test(Mechanical_test):
-    def __init__(self, infle):
-        super().__init__()
-        self.infle = infle
+class Resistance_mechanical_test(Mechanical_test):
+    def __init__(self):
+        super().__init__(self)
         self.maxF = 0
         self.idx = {'i': 0, 'f': 0, 'maxP': 0}
 
@@ -60,52 +65,70 @@ class Resistant_test(Mechanical_test):
         return self.data
 
     def get_max_load(self):
-        self.idx['maxF'] = np.argmax(self.data['F'])
-        self.maxF = np.max(self.data['F'])
+        if self.data.empty:
+            raise ValueError("No data loaded. Please load data before calculating the maximum load.")
+        else:
+            self.idx['maxF'] = np.argmax(self.data['F'])
+            self.maxF = np.max(self.data['F'])
 
         return self.maxF
 
     def preprocess_data(self):
         imaxP = self.idx['maxF']
-        self.idx['i'] = np.argmin(np.abs(self.data.loc[:imaxP, ['F']] - 0.01*self.maxF))
-        self.idx['f'] = np.argmin(np.abs(self.data.loc[imaxP:, ['F']] - 0.8*self.maxF)) + imaxP
+        self.idx['i'] = np.argmin(np.abs(self.data.loc[:imaxP, 'F'].to_numpy() - 0.01 * self.maxF))
+        self.idx['f'] = np.argmin(np.abs(self.data.loc[imaxP:, 'F'].to_numpy() - 0.8 * self.maxF)) + imaxP
 
         return self.idx
 
-class Axial_load_test(Mechanical_test):
-    def __init__(self, infle):
+class Axial_compression_test(Resistance_mechanical_test):
+    def __init__(self, sample_id=None, data_file= None, report_file=None):
         super().__init__()
-        self.infle = infle
+        self.sample_id = sample_id
+        self.report_file = report_file
+        self.data_file = data_file
+        self.area_sec = 0
+        self.strength = 0
 
-    def preprocess_data(self, withend=True):
-        F = np.abs(np.array(self.data['F']))
-        idx_maxF = np.argmax(F)
-        maxF = np.amax(F)
-        idx_i = np.argmin(np.abs(F[:idx_maxF] - 0.01*maxF))
+    def get_area_section(self, cell):
+        row, column = cell
+        diameter = get_report_data(file_path=self.report_file, sheet_idx=1, position=(row, column))
+        self.area_sec = np.pi*(diameter/2)**2
 
-        if withend:
-            idx_f = np.argmin(np.abs(F[idx_maxF:] - 0.8*maxF)) + idx_maxF
+        return self.area_sec
+    
+    def get_strength(self):
+        self.strength = self.maxF/self.area_sec
+
+        return self.strength
+
+class Axial_compression_test_report:
+    def __init__(self):
+        self.infle = 'infle'
+        self.folder= 'folder'
+        self.report_file = 'report_file'
+        self.report_data = pd.DataFrame()
+    
+    def add_test(self, test):
+        if isinstance(test, Axial_compression_test):
+            self.tests.append(test)
         else:
-            idx_f = F.shape[0] - 1
+            print("Only Axial_compression_test objects can be added to the report.")
 
-        data_array = self.data.iloc[idx_i:idx_f + 1,:].to_numpy()
+    def get_report_data(self):
+        report = []
+        for test in self.tests:
+            report.append([test.get_max_load() , test.get_area_section(cell=(1, 1)), test.get_strength()])
 
-        n = 0
-        for col in self.data.columns:
-            data_array[:, n] = np.abs(data_array[:, n])
-            
-            if col != 'F':
-                data_array[:, n] = data_array[:, n] - data_array[0, n]
+        self.report_data = pd.DataFrame(report, columns=['Max Load', 'Area Section', 'Strength'])
 
-            n += 1
-        
-        self.data_preprocess = pd.DataFrame(data_array, columns=self.data.columns)
-
-    def get_max_load(self):
-        F = np.abs(np.array(self.data_preprocess['F']))
-        D = np.abs(np.array(self.data_preprocess['D']))
-        idx_maxF = np.argmax(F)
-        maxF = np.amax(F)
-        D_maxF = D[idx_maxF]
-
-        return (idx_maxF, maxF, D_maxF)
+        return self.report_data
+    
+    def write_report(self):
+        for i, test in enumerate(self.tests):
+            row = i + 2
+            column = 1
+            write_report_data(file_path=self.report_file, sheet_idx=1, position=(row, column), val=test.maxF)
+            column = 2
+            write_report_data(file_path=self.report_file, sheet_idx=1, position=(row, column), val=test.area_sec)
+            column = 3
+            write_report_data(file_path=self.report_file, sheet_idx=1, position=(row, column), val=test.strength)
